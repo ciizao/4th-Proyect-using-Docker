e46f44482b9e4a2102b7beb71a00047a8849d1ee
ciizao <ciizao@uce.edu.ec>
2024-11-11 04:11:19 -0500

    feat: :sparkles: Create PHP code

---
+++ exercise4.php
@@ -0,0 +1,13 @@
+<?php
+$titulo = 'exercise4';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Exercise 4 PHP</title>
+</head>
+<body>
+    <h1><?php echo "EJERCICIO CON PHP EN DOCKER"; ?></h1>
+</body>
+</html>
